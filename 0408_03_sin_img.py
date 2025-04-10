112e6e95d1b83aabecbaa6a376bc3125463f3692
Linw <99076603+voke21@users.noreply.github.com>
2025-04-10 15:24:07 +0800

    0408_03_sin_img.py

--- 0408_03_sin_img.py
+++ 0408_03_sin_img.py
@@ -1,5 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.rcParams['font.sans-serif'] = ['SimHei']
+matplotlib.rcParams['axes.unicode_minus'] = False
 
 # 矩形滤波器
 ## x方向
@@ -14,9 +17,6 @@ def RectxFilter(m,n,d):
                 Filter[ii,jj] = 1
     return Filter
 
-# pic = RectxFilter(512,512,100)
-# plt.imshow(pic,cmap='gray')
-# plt.show()
 ## y方向
 def RectyFilter(m,n,d):
     Filter = np.zeros([m,n])
@@ -29,39 +29,50 @@ def RectyFilter(m,n,d):
                 Filter[ii,jj] = 1
     return Filter
 
-# pic = RectyFilter(512,512,100)
-# plt.imshow(pic,cmap='gray')
-# plt.show()
-
+# 插入待处理图像
 Img = plt.imread("Lena512.bmp",'gray')
 # print(Img.shape)
 m,n = Img.shape
 
+# 建立网格
 x = np.linspace(0,m,m)
 y = np.linspace(0,n,n)
 x,y = np.meshgrid(x,y)
 z = np.cos(20*x)
-
-# plt.subplot(121)
-# plt.imshow(abs(z),'gray')
-
-Nimg = Img + z * 2000
-
-# plt.subplot(122)
-# plt.imshow(abs(Nimg),'gray')
-# plt.show()
-
+## 调制图像
+## =====
+plt.figure(num=1, figsize=(12, 6))
 plt.subplot(121)
+plt.imshow(np.abs(z),'gray')
+plt.title("调制图")
+# 调制原图
+Nimg = Img + z * 100
+
+plt.subplot(122)
+plt.imshow(np.abs(Nimg),'gray')
+plt.title("调制后对象")
+plt.show()
+
+# 建立第二个窗口
+# =====
+plt.figure(num=2, figsize=(12, 6))
+# 调制后图像频谱
 fft_Img = np.fft.fft2(Nimg)
 fft_Img = np.fft.fftshift(fft_Img)
-# plt.imshow(np.log(1+abs(fft_Img)**2),'gray')
+plt.subplot(131)
+plt.imshow(np.log(1+abs(fft_Img)**2),'gray')
+plt.title("调制后对象频谱")
+# 构建带通滤波器
 d1 = 220
-d2 = 100
-Flt = 1-(RectxFilter(m,n,d1)-RectxFilter(m,n,d2))
-plt.imshow(Flt)
-
+d2 = 120
+Flt = 1-(RectyFilter(m,n,d1)-RectyFilter(m,n,d2))
+plt.subplot(132)
+plt.imshow(Flt,'gray')
+plt.title("滤波器")
+# 进行滤波
 fft_Img = fft_Img*Flt
 RSR_Img = np.fft.ifft2(fft_Img)
-plt.subplot(122)
+plt.subplot(133)
 plt.imshow(abs(RSR_Img),'gray')
+plt.title("滤波后图像")
 plt.show()
